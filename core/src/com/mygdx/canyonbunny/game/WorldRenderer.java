c1c48e3e5301e99d5f584bfea1a6b41a4c275e37
Adam Beltz <adambeltz@hotmail.com>
2017-02-21 13:34:14 -0600

    Added assets

--- core/src/com/mygdx/canyonbunny/game/WorldRenderer.java
+++ core/src/com/mygdx/canyonbunny/game/WorldRenderer.java
@@ -3,6 +3,7 @@ package com.mygdx.canyonbunny.game;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.Disposable;
 import com.mygdx.canyonbunny.util.Constants;
 
@@ -13,6 +14,8 @@ public class WorldRenderer implements Disposable {
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private WorldController worldController;
+    TextureAtlas textureAtlas;
+    Sprite coin;
 
     public WorldRenderer(WorldController worldController){
         this.worldController = worldController;
@@ -22,6 +25,10 @@ public class WorldRenderer implements Disposable {
 
     private void init(){
         batch = new SpriteBatch();
+        textureAtlas = new TextureAtlas("assets/canyonbunny.txt");
+        coin = textureAtlas.createSprite("item_gold_coin");
+        coin.setPosition(0,0);
+        coin.setSize(2,2);
         camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
         camera.position.set(0,0,0);
         camera.update();
@@ -35,6 +42,7 @@ public class WorldRenderer implements Disposable {
         worldController.cameraHelper.applyTo(camera);
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
+        coin.draw(batch);
         for (Sprite sprite : worldController.testSprites){
             sprite.draw(batch);
         }
