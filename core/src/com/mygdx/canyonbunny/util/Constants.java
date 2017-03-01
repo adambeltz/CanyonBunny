21bb1cb03f571e5b1f2159f53c1f75acdacb4027
Adam Beltz <adambeltz@hotmail.com>
2017-03-01 14:39:59 -0600

    added new version of texture atlas

--- core/src/com/mygdx/canyonbunny/util/Constants.java
+++ core/src/com/mygdx/canyonbunny/util/Constants.java
@@ -18,7 +18,7 @@ public class Constants {
     public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
 
     // Location of description file for texture atlas
-    public static final String TEXTURE_ATLAS_OBJECTS = "assets/canyonbunny.txt";
+    public static final String TEXTURE_ATLAS_OBJECTS = "assets/canyonbunny03.atlas";
 
     // Location of image file for level 01
     public static final String LEVEL_01 = "assets/levels/level-01.png";
